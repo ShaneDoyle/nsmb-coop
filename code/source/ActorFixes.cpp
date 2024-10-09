5c5df91f31c8d9b8f5c4dffb7f6c862f10b23cc7
TheGameratorT <tiagolsilva14@gmail.com>
2024-10-10 00:54:29 +0100

    Improve SpikeBass behavior

--- code/source/ActorFixes.cpp
+++ code/source/ActorFixes.cpp
@@ -32,6 +32,27 @@ NTR_USED static bool ActorFixes_isOutsideCamera(StageEntity* self, const FxRect&
 	return Stage::isOutsideCamera(self->position, boundingBox, player->linkedPlayerID);
 }
 
+static u32 ActorFixes_matchZoneID = -1;
+
+NTR_USED static Player* ActorFixes_getClosestPlayerFilter(s32 playerID)
+{
+	Player* player = Game::getPlayer(playerID);
+	if (player == nullptr || (ActorFixes_matchZoneID != -1 && !(scast<StageEntity*>(nullptr))->isPlayerInZone(*player, ActorFixes_matchZoneID)))
+		return nullptr;
+	return player;
+}
+
+ncp_set_call(0x020A0544, 0, ActorFixes_getClosestPlayerFilter)
+ncp_set_call(0x020A0628, 0, ActorFixes_getClosestPlayerFilter)
+
+NTR_USED static Player* ActorFixes_getClosestPlayerInZone(StageEntity* self, u32 zoneID)
+{
+	ActorFixes_matchZoneID = zoneID;
+	Player* player = ActorFixes_getClosestPlayer(self);
+	ActorFixes_matchZoneID = -1;
+	return player;
+}
+
 // Hammer/Fire/Boomerang Bros -----------------------------------------------------------
 
 ncp_over(0x021754F8, 56) const auto HammerBro_skipRender = ActorFixes_safeSkipRender;
@@ -304,26 +325,47 @@ ncp_repl(0x0217B8D0, 79, "LDRSB R0, [R0,#0x1E]") // Mega bump fix
 
 // Spike Bass ---------------------------------------------------------------------------
 
-asm(R"(
-ncp_call(0x02173318, 58)
-	MOV     R0, R4
-	B       _ZL27ActorFixes_getClosestPlayerP11StageEntity
-)");
+ncp_repl(0x021731B4, 58, ".int 0x4BC") // Add a zoneID field
 
-ncp_call(0x02173370, 58)
-bool SpikeBass_checkZoneForBothPlayers(StageEntity* self, Player* player, u32 zoneID)
+NTR_USED static Player* SpikeBass_fixGetClosestPlayer(StageEntity* self)
+{
+	u32 zoneID = rcast<u32*>(self)[0x4B8 / 4];
+	Player* player = ActorFixes_getClosestPlayerInZone(self, zoneID);
+	return player ? player : ActorFixes_getClosestPlayer(self);
+}
+
+ncp_set_call(0x02172CB0, 58, SpikeBass_fixGetClosestPlayer)
+ncp_set_call(0x02172E4C, 58, SpikeBass_fixGetClosestPlayer)
+
+NTR_USED static Player* SpikeBassSpawner_fixCheckPlayerInView(StageEntity* self)
 {
 	for (s32 playerID = 0; playerID < Game::getPlayerCount(); playerID++)
 	{
 		Player* player = Game::getPlayer(playerID);
-
-		if (self->isPlayerInZone(*player, zoneID))
+		if (player->viewID == self->viewID)
 		{
-			return true;
+			return player;
 		}
 	}
+	return nullptr;
+}
 
-	return false;
+asm(R"(
+ncp_call(0x02173318, 58)
+	MOV     R0, R4
+	B       _ZL37SpikeBassSpawner_fixCheckPlayerInViewP11StageEntity
+
+ncp_jump(0x021734C8, 58)
+	STR     R2, [R0,#0x4AC] // Keep replaced instruction
+	LDRB    R2, [R4,#0x402] // R2 = SpikeBassSpawner*->zoneID
+	STR     R2, [R0,#0x4B8] // SpikeBass*->zoneID = R2
+	B       0x021734CC      // Return to code
+)");
+
+ncp_call(0x02173370, 58)
+bool SpikeBassSpawner_fixCheckPlayerInZone(StageEntity* self, Player* player, u32 zoneID)
+{
+	return ActorFixes_getClosestPlayerInZone(self, zoneID) != nullptr;
 }
 
 // Misc ---------------------------------------------------------------------------------
