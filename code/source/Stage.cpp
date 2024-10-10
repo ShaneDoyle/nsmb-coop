6370a5b2630d47ff27af8fb210643bc3618d6777
TheGameratorT <tiagolsilva14@gmail.com>
2024-10-10 02:52:51 +0100

    Rewrite fading fix

--- code/source/Stage.cpp
+++ code/source/Stage.cpp
@@ -8,6 +8,7 @@
 #include "nsmb/system/function.h"
 #include "nsmb/system/misc.h"
 #include "nsmb/graphics/fader.h"
+#include "nsmb/entity/scene.h"
 
 #include "PlayerSpectate.hpp"
 #include "util/eprintf.h"
@@ -118,9 +119,18 @@ void call_020203EC() // When Mario gets 1-up from coins, also give Luigi 1-up.
 ncp_repl(0x020D13B4, 10, "NOP") // Powerups don't despawn
 ncp_repl(0x0209B7C0, 0, "NOP") // Permanently destroyed entities do not respawn
 
-ncp_repl(0x020A3430, 0, "MOV R0, #1") // Fix Scene::preCreate getting called 4 times (fixes fading)
+ncp_repl(0x0201378C, "MOV R0, R4") // Pass Scene* instead of &Game::fader to Stage_fixFadingSetup
 
-ncp_repl(0x0209AEEC, 0, "MOV R3, #0") // Prevent StageEntity::isOutOfView from returning "true" locally for the player that dies (fixes some enemies despawning on player death)
+ncp_call(0x02013798)
+void Stage_fixFadingSetup(Scene* self, Fader::FadingType type, bool fadeBright, bool staticBlend)
+{
+	if (self->id == scast<u16>(SceneID::Stage) && rcast<u32*>(self)[0x640C / 4])
+		return;
+	Game::fader.setupSceneFading(type, fadeBright, staticBlend);
+}
+
+//ncp_repl(0x0209AEEC, 0, "MOV R3, #0") // Prevent StageEntity::isOutOfView from returning "true" locally for the player that dies (fixes some enemies despawning on player death)
+//ncp_repl(0x0209AE88, 0, "MOV R0, #0; BX LR")
 
 // No idea what these do
 // ncp_repl(0x0209B254, 0, "MOV R0, #1")
