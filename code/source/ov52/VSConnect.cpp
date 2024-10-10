4908c186e77e45a943a95d65e9d2617a95c7b384
TheGameratorT <tiagolsilva14@gmail.com>
2024-10-10 11:54:41 +0100

    Update code style in VSConnect

--- code/source/ov52/VSConnect.cpp
+++ code/source/ov52/VSConnect.cpp
@@ -6,21 +6,21 @@
 ncp_call(0x021592DC, 52)
 u32 VSConnect_skipFirstSubMenu()
 {
-	register VSConnectScene* _this asm("r4");
+	register VSConnectScene* self asm("r4");
 
-	((u32*)_this)[0x15C / 4] = (u32)&VSConnectScene::charSelectSM; // Sub-menu updater
-	((u32*)_this)[0x160 / 4] = 1; // Sub-menu swap timer
+	rcast<u32*>(self)[0x15C / 4] = rcast<u32>(&VSConnectScene::charSelectSM); // Sub-menu updater
+	rcast<u32*>(self)[0x160 / 4] = 1; // Sub-menu swap timer
 
 	return 1;
 }
 
 ncp_call(0x02158800, 52)
-void VSConnect_modifyReturn(VSConnectScene* _this, VSConnectScene::SubMenu* subMenu, s32 delay, bool playSound)
+void VSConnect_modifyReturn(VSConnectScene* self, VSConnectScene::SubMenu* subMenu, s32 delay, bool playSound)
 {
-	_this->scheduleSubMenuChange(subMenu, delay, playSound); // Call replaced function
+	self->scheduleSubMenuChange(subMenu, delay, playSound); // Call replaced function
 
 	// Variable that happens to not be 0 during connection menu
-	if (((u32*)_this)[0x134 / 4])
+	if (rcast<u32*>(self)[0x134 / 4])
 	{
 		Scene::switchScene(SceneID::TitleScreen, 0);
 		Sound::stopBGM(30);
