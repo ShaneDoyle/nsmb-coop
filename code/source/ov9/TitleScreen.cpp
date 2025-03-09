d93da185fa4449daf2af2494a6776981cf2405f3
TheGameratorT <tiagolsilva14@gmail.com>
2025-03-09 15:13:25 +0000

    Save widescreen setting

--- code/source/ov9/TitleScreen.cpp
+++ code/source/ov9/TitleScreen.cpp
@@ -3,7 +3,19 @@
 #include <nsmb/core/system/input.hpp>
 #endif
 
+#include <nsmb/game/sound.hpp>
+#include <nsmb/core/system/save.hpp>
+
 #include "Widescreen.hpp"
+
+asm(R"(
+	TitleScreen_onCreate = 0x020D3570
+	fun020CD884 = 0x020CD884
+)");
+extern "C" {
+	s32 TitleScreen_onCreate(void*);
+	void fun020CD884(void*);
+}
 
 // Skip MvsL and Minigames buttons
 ncp_repl(0x020D317C, 9, "ADDNE R1, R1, #3")
@@ -22,12 +34,18 @@ u32 TitleScreen_getSceneIDAfterFileSelect()
 ncp_repl(0x020CD700, 9, "MOV R0, #6") // Bowser Jr. Intro to MvsL Menu
 ncp_repl(0x020D3708, 9, "MOV R1, #0") // MvsL returns to Main Menu button 0
 
-// Widescreen toggle
+s32 TitleScreen_onCreate_ext(void* self)
+{
+	s32 result = TitleScreen_onCreate(self);
 
-asm(R"(
-	fun020CD884 = 0x020CD884
-)");
-extern "C" void fun020CD884(void*);
+	Widescreen::enabled[0] = Save::optionSave.flags & (1 << 1);
+
+	return result;
+}
+
+ncp_repl(0x020DAA94, 9, ".int _Z24TitleScreen_onCreate_extPv")
+
+// Widescreen toggle
 
 ncp_call(0x020D3474, 9)
 void TitleScreen_updateHook(void* r0)
@@ -40,7 +58,19 @@ void TitleScreen_updateHook(void* r0)
 	{
 		if (Input::consoleKeys[0].pressed & Keys::X)
 		{
-			Widescreen::enabled[0] = !Widescreen::enabled[0];
+			if (Widescreen::enabled[0])
+			{
+				Widescreen::enabled[0] = false;
+				Save::optionSave.flags &= ~(1 << 1);
+				SND::playSFX(230);
+			}
+			else
+			{
+				Widescreen::enabled[0] = true;
+				Save::optionSave.flags |= (1 << 1);
+				SND::playSFX(231);
+			}
+			Save::writeOptionSave(&Save::optionSave);
 		}
 	}
 }
